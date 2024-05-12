70089b50290ca52f149411c201a8c1b45c1dd279
Kyle Jacobson <jacokyle01@gmail.com>
2024-05-12 13:33:29 -0500

    implement walk through trainable positions

--- src/ctrl.ts
+++ src/ctrl.ts
@@ -2,24 +2,65 @@ import { Api } from 'chessground/api';
 import { Redraw } from './types';
 import { Chess } from 'chess.js';
 import { ChessSrs } from 'chess-srs';
-import { Color } from 'chess-srs/dist/types';
+import { Color, TrainingData } from 'chess-srs/dist/types';
 
 export default class PrepCtrl {
-  count = 0;
+  subrepertoireNames: string[] = [];
 
   //libraries
   chessground: Api | undefined;
   chessSrs = ChessSrs();
   chess: Chess = new Chess();
 
-  constructor(readonly redraw: Redraw) {}
+  constructor(readonly redraw: Redraw) {
+    //we are initially learning
+    this.chessSrs.setMethod('learn');
+  }
 
-  increment = () => {
-    this.count++;
+  //TODO PGN validation
+  addSubrepertoire = (pgn: string, name: string, color: Color = 'white') => {
+    this.chessSrs.addSubrepertoires(pgn, color);
+    this.subrepertoireNames.push(name);
+    console.log(this.subrepertoireNames);
+    this.redraw();
+    console.log(this.chessSrs.state());
+  };
+
+  selectSubrepertoire = (which: number) => {
+    this.chessSrs.load(which);
     this.redraw();
   };
 
-  addSubrepertoire = (pgn: string, color: Color = 'white') => {
-    this.chessSrs.addSubrepertoires(pgn, color);
+  //advance subrepertoire to next trainable position and
+  handleNext = () => {
+    this.chessSrs.load(0);
+    //TODO handle null
+    this.chessSrs.next();
+    console.log(this.chessSrs.state().path);
+    this.setPgn(this.chessSrs.state().path!);
+
   };
+
+  //TODO fix. correct typing
+  //we use the Chess.js library to load a PGN into memory
+  //there are 2 reasons for using this library
+  //a) move validation
+  //b) deriving a FEN from a PGN 
+
+  setPgn = (path: any) => {
+    const pgn = path.map((node: { data: { san: any } }) => node.data.san).join(' ');
+    console.log(pgn);
+    this.chess.loadPgn(pgn);
+    const fen = this.getFen();
+    this.chessground?.set(
+      {
+        fen: fen
+      }
+    )
+    this.redraw();
+  };
+
+  getFen = () => {
+    return this.chess.fen(); 
+  }
 }
--- src/view.ts
+++ src/view.ts
@@ -2,27 +2,57 @@ import { VNode, h } from 'snabbdom';
 import PrepCtrl from './ctrl';
 import { chessground } from './chessground';
 
+const next = (ctrl: PrepCtrl) => {
+  return h('button#next', { on: { click: () => ctrl.handleNext() } }, 'Next');
+};
+
 const addSubrepertoire = (ctrl: PrepCtrl): VNode => {
   return h('div#add-subrepertoire-wrap', [
     h('h2', 'add subrepertoire'),
-    h('textarea'),
+    h(
+      'textarea#pgnInput',
+      '1. e4 e5 2. f4 exf4 (2... d5) 3. Nf3 (3. Bc4 Qh4+ (3... d5) 4. Kf1) (3. d4 Qh4+ 4. Ke2) 3... d6 { test } (3... g5)',
+    ),
+    h('h2', 'choose name'),
+    h('textarea#nameInput', 'test'),
     h('button', {
       on: {
         click: () => {
-          const pgn = document.querySelector('textarea')!;
-          ctrl.addSubrepertoire(pgn.value);
+          const pgn = document.getElementById('pgnInput')! as HTMLTextAreaElement;
+          const name = document.getElementById('nameInput')! as HTMLTextAreaElement;
+          ctrl.addSubrepertoire(pgn.value, name.value);
         },
       },
     }),
   ]);
 };
 
+const subrepertoireTree = (ctrl: PrepCtrl): VNode => {
+  return h(
+    'div#subrepertoire-tree-wrap',
+    ctrl.subrepertoireNames.map((name, index) =>
+      h(
+        'div',
+        {
+          on: {
+            click: () => ctrl.selectSubrepertoire(index),
+          },
+          class: {
+            selected: ctrl.chessSrs.state().index == index,
+          },
+        },
+        name,
+      ),
+    ),
+  );
+};
+
 const view = (ctrl: PrepCtrl): VNode => {
   return h('div#main', [
-    h('button#clicker', { on: { click: () => ctrl.increment() } }),
-    h('div#count', ctrl.count),
     h('div', chessground(ctrl)),
     addSubrepertoire(ctrl),
+    subrepertoireTree(ctrl),
+    next(ctrl),
   ]);
 };
 export default view;
