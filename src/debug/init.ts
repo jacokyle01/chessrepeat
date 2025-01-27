af61205f6f17d70882172e17e59448f5b1413b33
Kyle Jacobson <jacokyle01@gmail.com>
2025-01-26 21:05:51 -0600

    add default repertoires

--- src/debug/init.ts
+++ src/debug/init.ts
@@ -1,26 +1,15 @@
 import PrepCtrl from '../ctrl';
-import { pgn1, pgn2, pgn3, pgn4, pgn5, pgn6 } from './pgns';
+import { pgn1, pgn2} from './pgns';
 
 export const init = (ctrl: PrepCtrl) => {
   // initial zoom
   document.body.style.setProperty('--zoom', '100');
 
   // add 3 repertoire entries
-  ctrl.addToRepertoire(pgn1(), 'white', 'Catalan');
-  ctrl.addToRepertoire(pgn2(), 'white', 'Catalan 2');
-  // ctrl.addToRepertoire(pgn3(), 'white', 'Spanish');
-  ctrl.addToRepertoire(pgn4(), 'black', 'Englund');
-  ctrl.addToRepertoire(pgn6(), 'black', 'Alekhine');
-  // ctrl.addToRepertoire(pgn5(), 'black', 'Classical Sicilian');
+  ctrl.addToRepertoire(pgn1(), 'black', 'Caro-Kann');
+  ctrl.addToRepertoire(pgn2(), 'white', 'King\'s Gambit');
 
-  // train Catalan
-  ctrl.selectSubrepertoire(0);
-  for (let i = 0; i < 4; i++) {
-    ctrl.getNext();
-    ctrl.succeed();
-  }
-  ctrl.syncTime();
-  ctrl.handleRecall();
+
   ctrl.chessground?.redrawAll();
 ctrl.redraw();
 };
