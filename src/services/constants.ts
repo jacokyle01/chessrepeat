f8b2f82a2c5e45f8a62c79fc2c70302661cab128
Kyle Jacobson <jacokyle01@gmail.com>
2025-07-18 16:01:46 -0500

    .

--- src/services/constants.ts
+++
@@ -1,7 +0,0 @@
-export const baseUrl = 'http://52.14.126.128:8080';
-export const lichessHost = 'https://lichess.org';
-export const here = (() => {
-  const url = new URL(location.href);
-  url.search = '';
-  return url.href;
-})();
