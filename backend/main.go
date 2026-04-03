74e2e800775b4b6c614066eb5de644b6d9956c8d
ben nguyen <ben706519@fastmail.com>
2026-04-03 13:12:06 -0500

    setting up REST

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,23 @@
 package main
 
 import "net/http"
+import "log"
 import "fmt"
 
 func main() {
   fmt.Println("starting server...")
 
-  http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
+  http.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+    if (r.Method == "GET") {
+      id := r.PathValue("id")
+      fmt.Println("fetching repertoire for id:", id)
+      w.WriteHeader(http.StatusOK)
+    } else {
+      w.WriteHeader(http.StatusMethodNotAllowed)
+    }
   })
 
-  fmt.Println("server ready to serve!")
+  fmt.Println("server ready to serve! http://localhost:8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
