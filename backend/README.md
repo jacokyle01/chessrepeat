3cc5fb825018d4da4afc425dc8cbe4cf1924c571
Kyle Jacobson <jacokyle01@gmail.com>
2026-04-03 20:20:48 -0500

    load from .env

--- backend/README.md
+++ backend/README.md
@@ -1,6 +1,6 @@
 ### run
 set DBUSER and DBPASS in env
-
+create chessrepeat database
 ```
 ./build.sh && ./chessrepeat
 ```
--- backend/main.go
+++ backend/main.go
@@ -7,149 +7,155 @@ import "os"
 import "database/sql"
 import "github.com/go-sql-driver/mysql"
 import "github.com/google/uuid"
+import "github.com/joho/godotenv"
 
 func connectDb() *sql.DB {
-  log.Println("fetching config...")
+	log.Println("fetching config...")
 
-  dbuser := os.Getenv("DBUSER")
-  dbpass := os.Getenv("DBPASS")
+	dbuser := os.Getenv("DBUSER")
+	dbpass := os.Getenv("DBPASS")
 
-  cfg := mysql.NewConfig()
-  cfg.User = dbuser
-  cfg.Passwd = dbpass
-  cfg.Net = "unix"
-  cfg.Addr = "/tmp/mysql.sock"
-  cfg.DBName = "chessrepeat"
+	cfg := mysql.NewConfig()
+	cfg.User = dbuser
+	cfg.Passwd = dbpass
+	cfg.Net = "unix"
+	cfg.Addr = "/tmp/mysql.sock"
+	cfg.DBName = "chessrepeat"
 
-  log.Println("setting up connection...")
+	log.Println("setting up connection...")
 
-  var db *sql.DB
-  var err error
-  db, err = sql.Open("mysql", cfg.FormatDSN())
-  if err != nil {
-    log.Fatal(err)
-  }
+	var db *sql.DB
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  log.Println("testing connection...")
+	log.Println("testing connection...")
 
-  pingErr := db.Ping()
-  if pingErr != nil {
-    log.Fatal(pingErr)
-  }
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
 
-  log.Println("connected to database!")
+	log.Println("connected to database!")
 
-  return db;
+	return db
 }
 
 func isValidName(name string) bool {
-  return name != ""
+	return name != ""
 }
 
 func isValidTrainAs(trainAs string) bool {
-  return trainAs == "white" || trainAs == "black"
+	return trainAs == "white" || trainAs == "black"
 }
 
 func main() {
-  log.Println("starting server...")
 
-  var db = connectDb();
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+  
+	log.Println("starting server...")
 
-  http.HandleFunc("/repertoire/{id}", func(w http.ResponseWriter, r *http.Request) {
-    if (r.Method == "GET") {
-      unparsedId := r.PathValue("id")
-      var id, err = uuid.Parse(unparsedId)
-      if (err != nil) {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-      }
+	var db = connectDb()
 
-      type repertoireJson struct {
-        RepertoireId *string `json:"id"`
-        Name *string `json:"name"`
-        TrainAs *string `json:"trainAs"`
-      }
+	http.HandleFunc("/repertoire/{id}", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			unparsedId := r.PathValue("id")
+			var id, err = uuid.Parse(unparsedId)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
 
-      log.Println("fetching repertoire for id:", id)
+			type repertoireJson struct {
+				RepertoireId *string `json:"id"`
+				Name         *string `json:"name"`
+				TrainAs      *string `json:"trainAs"`
+			}
 
-      row := db.QueryRow("SELECT * FROM repertoire WHERE repertoire_id=?", id)
+			log.Println("fetching repertoire for id:", id)
 
-      var repertoire repertoireJson
-      err = row.Scan(&repertoire.RepertoireId, &repertoire.Name, &repertoire.TrainAs)
-      if (err != nil) {
-        // TODO(ben): make not found 404s
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-      }
+			row := db.QueryRow("SELECT * FROM repertoire WHERE repertoire_id=?", id)
 
-      w.Header().Set("Content-Type", "application/json")
-      json.NewEncoder(w).Encode(repertoire)
+			var repertoire repertoireJson
+			err = row.Scan(&repertoire.RepertoireId, &repertoire.Name, &repertoire.TrainAs)
+			if err != nil {
+				// TODO(ben): make not found 404s
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 
-      w.WriteHeader(http.StatusOK)
-    } else if (r.Method == "POST") {
-      unparsedId := r.PathValue("id")
-      var id, err = uuid.Parse(unparsedId)
-      if (err != nil) {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-      }
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(repertoire)
 
-      type repertoireJson struct {
-        Name *string `json:"name"`
-        TrainAs *string `json:"trainAs"`
-      }
+			w.WriteHeader(http.StatusOK)
+		} else if r.Method == "POST" {
+			unparsedId := r.PathValue("id")
+			var id, err = uuid.Parse(unparsedId)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
 
-      decoder := json.NewDecoder(r.Body)
-      var repertoire repertoireJson
-      err = decoder.Decode(&repertoire)
-      if err != nil {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-      }
-      if (!isValidName(*repertoire.Name)) {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-      }
-      if (!isValidTrainAs(*repertoire.TrainAs)) {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-      }
+			type repertoireJson struct {
+				Name    *string `json:"name"`
+				TrainAs *string `json:"trainAs"`
+			}
 
-      log.Println("creating repertoire for id:", id)
+			decoder := json.NewDecoder(r.Body)
+			var repertoire repertoireJson
+			err = decoder.Decode(&repertoire)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if !isValidName(*repertoire.Name) {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			if !isValidTrainAs(*repertoire.TrainAs) {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
 
-      tx, err := db.Begin()
-      if err != nil {
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-      }
-      stmt, err := tx.Prepare("INSERT INTO repertoire(repertoire_id, name, train_as) VALUES (?, ?, ?)")
-      if err != nil {
-        tx.Rollback()
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-      }
-      _, err = stmt.Exec(id, *repertoire.Name, *repertoire.TrainAs)
-      if err != nil {
-        tx.Rollback()
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-      }
-      err = tx.Commit()
-      if err != nil {
-        tx.Rollback()
-        w.WriteHeader(http.StatusInternalServerError)
-        return
-      }
-      stmt.Close()
+			log.Println("creating repertoire for id:", id)
 
-      w.WriteHeader(http.StatusOK)
-    } else {
-      w.WriteHeader(http.StatusMethodNotAllowed)
-    }
-  })
+			tx, err := db.Begin()
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			stmt, err := tx.Prepare("INSERT INTO repertoire(repertoire_id, name, train_as) VALUES (?, ?, ?)")
+			if err != nil {
+				tx.Rollback()
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			_, err = stmt.Exec(id, *repertoire.Name, *repertoire.TrainAs)
+			if err != nil {
+				tx.Rollback()
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			err = tx.Commit()
+			if err != nil {
+				tx.Rollback()
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			stmt.Close()
 
-  log.Println("server ready to serve! http://localhost:8080")
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("server ready to serve! http://localhost:8080")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
